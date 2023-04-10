a3a668e97e7e13bc027b0b5c8ce7c824add27d24
John Galt <galt@fedora.com>
2023-04-10 23:42:37 +0200

    module imports

---
+++ official_doc/11-module-imports/src/01_import_general.v
@@ -0,0 +1,8 @@
+import os
+
+fn main() {
+	// read text from stdin
+	name := os.input('Enter your name: ')
+	println('Hello, ${name}')
+}
+
---
+++ official_doc/11-module-imports/src/02_import_selective.v
@@ -0,0 +1,9 @@
+import os { input, user_os }
+
+name := input("Enter your name: ")
+println("Name: ${name}")
+
+current_os := user_os()
+println("Your OS is ${current_os}")
+
+
